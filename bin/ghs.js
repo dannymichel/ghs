da7b2a3cbfd806b68a1c3b1e01caf74744a158b9
dannymichel <dan@varmag.com>
2024-05-31 02:31:25 -0400

    delete option

--- bin/ghs.js
+++ bin/ghs.js
@@ -2,7 +2,7 @@
 
 const yargs = require('yargs/yargs');
 const { hideBin } = require('yargs/helpers');
-const { exportAndAnalyzeGpgKeys, saveKeys, setDefaultKeys, createNewKeys } = require('../lib/gpg');
+const { exportAndAnalyzeGpgKeys, saveKeys, setDefaultKeys, createNewKeys, deleteKeys } = require('../lib/gpg');
 const { cloneRepo, switchAccount } = require('../lib/git');
 const { promptClone, promptSwitch } = require('../lib/prompts');
 
@@ -20,6 +20,9 @@ const argv = yargs(hideBin(process.argv))
     })
     .command('new', 'Create a new GPG and SSH key pair', {}, async (args) => {
         await createNewKeys(args.dryRun);
+    })
+    .command('delete', 'Delete a saved GPG and SSH key pair configuration', {}, async (args) => {
+        await deleteKeys(args.dryRun);
     })
     .command('clone', 'Clone a repository', {}, async (args) => {
         const cloneDetails = await promptClone();
