2c6f11614a33def29cf414adf7529533e26192b2
dannymichel <dan@varmag.com>
2024-06-05 06:09:35 -0400

    complete front-end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -48,16 +48,28 @@ const App = () => {
         try {
             let url = `http://localhost:9000/${command}`;
             let data = {};
+            let method = 'post';
+            if (command === 'list' || command === 'analyze') {
+                method = 'get';
+            }
+
             if (command === 'clone') {
                 data = { name: selectedConfig, url: repositoryUrl };
             } else if (command === 'save') {
                 data = { name: selectedConfig, gpgKey, sshKey };
+            } else if (command === 'new') {
+                data = { name: selectedConfig, gpgName: gpgKey, gpgEmail: sshKey, gpgPassphrase: 'dummyPass', sshKeyName: 'dummySshKey' };
             } else {
                 data = { name: selectedConfig };
             }
 
-            const response = await axios.post(url, data);
-            setResponse(response.data);
+            const response = method === 'post' ? await axios.post(url, data) : await axios.get(url);
+
+            if (command === 'list') {
+                setResponse(JSON.stringify(response.data, null, 2));
+            } else {
+                setResponse(`Executed ${command} successfully.`);
+            }
         } catch (error) {
             console.error('Error executing command:', error);
             setResponse(`Error executing command: ${error.message}`);
@@ -93,7 +105,7 @@ const App = () => {
                                 onChange={(e) => setCommand(e.target.value)}
                                 fullWidth
                             >
-                                <MenuItem value="configs">List Configs</MenuItem>
+                                <MenuItem value="list">List Configs</MenuItem>
                                 <MenuItem value="clone">Clone</MenuItem>
                                 <MenuItem value="switch">Switch</MenuItem>
                                 <MenuItem value="delete">Delete</MenuItem>
@@ -105,7 +117,7 @@ const App = () => {
                             </Select>
                         </FormControl>
 
-                        {(command === 'clone' || command === 'save') && (
+                        {(command === 'clone' || command === 'save' || command === 'new' || command === 'switch' || command === 'delete' || command === 'export' || command === 'default') && (
                             <Box>
                                 <FormControl fullWidth margin="normal">
                                     <InputLabel>Configuration</InputLabel>
@@ -146,23 +158,54 @@ const App = () => {
                                         />
                                     </Box>
                                 )}
+                                {command === 'new' && (
+                                    <Box>
+                                        <TextField
+                                            label="GPG Name"
+                                            value={gpgKey}
+                                            onChange={(e) => setGpgKey(e.target.value)}
+                                            fullWidth
+                                            margin="normal"
+                                        />
+                                        <TextField
+                                            label="GPG Email"
+                                            value={sshKey}
+                                            onChange={(e) => setSshKey(e.target.value)}
+                                            fullWidth
+                                            margin="normal"
+                                        />
+                                        <TextField
+                                            label="GPG Passphrase"
+                                            value="dummyPass"
+                                            disabled
+                                            fullWidth
+                                            margin="normal"
+                                        />
+                                        <TextField
+                                            label="SSH Key Name"
+                                            value="dummySshKey"
+                                            disabled
+                                            fullWidth
+                                            margin="normal"
+                                        />
+                                    </Box>
+                                )}
                             </Box>
                         )}
 
                         <Button variant="contained" color="primary" onClick={executeCommand}>Execute</Button>
+                        {response && (
+                            <Box mt={2}>
+                                <Typography variant="body1">Response</Typography>
+                                <pre>{response}</pre>
+                            </Box>
+                        )}
                     </Paper>
                 </Grid>
 
                 <Grid item xs={12}>
                     <Paper elevation={3} style={{ padding: '16px' }}>
-                        <Typography variant="h6">Response</Typography>
-                        <pre>{response}</pre>
-                    </Paper>
-                </Grid>
-
-                <Grid item xs={12}>
-                    <Paper elevation={3} style={{ padding: '16px' }}>
-                        <Typography variant="h6">Saved Configurations</Typography>
+                        <Typography variant="h6">Existing Configurations</Typography>
                         <ul>
                             {configs.map((config, index) => (
                                 <li key={index}>
